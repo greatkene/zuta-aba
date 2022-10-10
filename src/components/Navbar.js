24762cc512045db4a14e52df47fde9b6c158c5c2
greatkene <great.ihevueme@tekparadise.com>
2022-10-10 12:14:28 +0100

    nav setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,30 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to="/">
+            <img src={logo} alt='zuta' />
+          </Link>
+          <button type='button' className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          {links.map((link) => {
+            const { id, text, url} = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </NavContainer>
+  )
 }
 
 const NavContainer = styled.nav`
@@ -28,7 +51,7 @@ const NavContainer = styled.nav`
     align-items: center;
     justify-content: space-between;
     img {
-      width: 175px;
+      width: 115px;
       margin-left: -15px;
     }
   }
