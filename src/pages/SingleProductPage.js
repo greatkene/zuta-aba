d8d2028553a8bdb694efa88f4d50b9e456b74dc8
greatkene <great.ihevueme@tekparadise.com>
2022-10-24 10:21:58 +0100

    update: single page basic setup

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -43,8 +43,46 @@ const SingleProductPage = () => {
   if(error) {
     return <Error />
   }
-
-  return <h4>single product page</h4>
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product
+  
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className='section section-center pgae'>
+        <Link to="/products" className='btn'>Back To Products</Link>
+        <div className='product-center'>
+          <ProductImages />
+          <section className='content'>
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className='price'>{formatPrice(price)}</h5>
+            <p className='desc'>{description}</p>
+            <p className='info'>
+              <span>Available: </span> {stock > 0 ? 'In Stock' : 'Out of Stock'}
+            </p>
+            <p className='info'>
+              <span>SKU: </span> {sku}
+            </p>
+            <p className='info'>
+              <span>Brand: </span> {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.main`
