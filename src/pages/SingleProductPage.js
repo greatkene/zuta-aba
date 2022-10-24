6bcd26f5c34dc399c56e77649eaa4e3e1239b11f
greatkene <great.ihevueme@tekparadise.com>
2022-10-24 09:47:21 +0100

    feat: fetch single product setup

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,6 +15,35 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
+  const {id} = useParams()
+  const history = useHistory()
+  const {
+    single_product_loading: loading, 
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct
+  } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+  }, [id])
+
+  // Redirect to home page after 3 sec
+  useEffect(() => {
+    if(error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+  }, [error])
+
+  if(loading) {
+    return <Loading />
+  }
+  if(error) {
+    return <Error />
+  }
+
   return <h4>single product page</h4>
 }
 
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -19,7 +19,7 @@ const products_reducer = (state, action) => {
   if( action.type === GET_PRODUCTS_BEGIN ) {
     return {...state, products_loading: true}
   }
-  if (action.type === GET_PRODUCTS_SUCCESS) {
+  if(action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter(
       (product) => product.featured === true
     )
@@ -30,8 +30,29 @@ const products_reducer = (state, action) => {
       featured_products,
     }
   }
-  if (action.type === GET_PRODUCTS_ERROR) {
+  if(action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true }
+  }
+  if(action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state, 
+      single_product_loading: true, 
+      single_product_error: false
+    }
+  }
+  if(action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state, 
+      single_product_loading:false, 
+      single_product: action.payload
+    }
+  }
+  if(action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state, 
+      single_product_loading: false, 
+      single_product_error: true
+    }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
