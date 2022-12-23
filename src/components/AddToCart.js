4061325e3d1f57865449fb40caf9844715afe8a4
greatkene <great.ihevueme@tekparadise.com>
2022-12-23 12:46:21 +0100

    feat: increase and decrease of cart number

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -4,13 +4,33 @@ import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
-import minus from "../assets/icon-minus.svg"
-import plus from "../assets/icon-plus.svg"
-import cart from "../assets/icon-cart-white.svg"
+
 
 const AddToCart = ({product}) => {
   const {id, stock, colors} = product
   const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if(tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if(tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
+
   return (
    <Wrapper>
      <div className='colors'>
@@ -34,23 +54,9 @@ const AddToCart = ({product}) => {
           })
         }
       </div>
-     </div>
-      <Buttons>
-      <div className='quantity'> 
-        <div className='dec'>
-          <img src={minus} alt={minus} />
-        </div>
-        <div className='current-quantity'>4</div>
-        <div className='inc'>
-          <img src={plus} alt={plus} />
-        </div>
-      </div>
-      <div className='add-to-cart'>
-        <button>
-          <img src={cart} alt={cart} /> Add to cart
-        </button>
-      </div>
-    </Buttons>
+    </div>
+    <AmountButtons decrease={decrease} increase={increase} amount={amount} />
+      
    </Wrapper>
   )
 }
@@ -101,87 +107,5 @@ const Wrapper = styled.section`
   }
 `
 
-const Buttons = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  .quantity {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color: hsl(223, 64%, 98%);
-    width: fit-content;
-    height: 55px;
-    width: 160px;
-    border-radius: 10px;
-    user-select: none;
-    margin-right: 20px;
-    .current-quantity {
-      font-weight: 700;
-      width: 50px;
-      text-align: center;
-    }
-    .dec {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 25px;
-      width: 25px;
-      padding: 25px;
-      cursor: pointer;
-      &:hover {
-        opacity: 50%;
-        transition: 100ms ease;
-      }
-    }
-    .inc {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 25px;
-      width: 25px;
-      padding: 25px;
-      cursor: pointer;
-      &:hover {
-        opacity: 50%;
-        transition: 100ms ease;
-      }
-    }
-  }
-  .add-to-cart {
-    button {
-      height: 55px;
-      width: 250px;
-      background-color: hsl(26, 100%, 55%);
-      border: none;
-      border-radius: 10px;
-      align-items: center;
-      display: flex;
-      justify-content: center;
-      img {
-        margin-right: 10px;
-      }
-      color: white;
-      font-weight: 700;
-      cursor: pointer;
-      transition: 100ms ease;
-      &:hover {
-        background-color: hsl(26, 100%, 55%, 50%);
-      }
-    }
-  }
-  @media (max-width: 499px) {
-    .quantity {
-      width: 100%;
-      margin-bottom: 20px;
-      margin-right: 0;
-    }
-    .add-to-cart {
-      width: 100%;
-      button {
-        width: 100%;
-      }
-    }
-  }
-`;
 
 export default AddToCart
--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -2,33 +2,113 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
-const AmountButtons = () => {
-  return <h4>amount buttons </h4>
+import minus from "../assets/icon-minus.svg"
+import plus from "../assets/icon-plus.svg"
+import cart from "../assets/icon-cart-white.svg"
+
+const AmountButtons = ({increase, decrease, amount}) => {
+  return (
+    <Buttons>
+    <div className='quantity'> 
+      <div onClick={decrease} className='dec'>
+        <img src={minus} alt={minus} />
+      </div>
+      <div className='current-quantity'>{amount}</div>
+      <div onClick={increase} className='inc'>
+        <img src={plus} alt={plus} />
+      </div>
+    </div>
+    <div className='add-to-cart'>
+      <button>
+        <img src={cart} alt={cart} /> Add to cart
+      </button>
+    </div>
+  </Buttons>
+  )
 }
 
-const Wrapper = styled.div`
-  display: grid;
-  width: 140px;
-  justify-items: center;
-  grid-template-columns: repeat(3, 1fr);
-  align-items: center;
-  h2 {
-    margin-bottom: 0;
-  }
-  button {
-    background: transparent;
-    border-color: transparent;
-    cursor: pointer;
-    padding: 1rem 0;
-    width: 2rem;
-    height: 1rem;
+const Buttons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  .quantity {
     display: flex;
+    justify-content: space-around;
     align-items: center;
-    justify-content: center;
+    background-color: hsl(223, 64%, 98%);
+    width: fit-content;
+    height: 55px;
+    width: 160px;
+    border-radius: 10px;
+    user-select: none;
+    margin-right: 20px;
+    .current-quantity {
+      font-weight: 700;
+      width: 50px;
+      text-align: center;
+    }
+    .dec {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 25px;
+      width: 25px;
+      padding: 25px;
+      cursor: pointer;
+      &:hover {
+        opacity: 50%;
+        transition: 100ms ease;
+      }
+    }
+    .inc {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 25px;
+      width: 25px;
+      padding: 25px;
+      cursor: pointer;
+      &:hover {
+        opacity: 50%;
+        transition: 100ms ease;
+      }
+    }
   }
-  h2 {
-    margin-bottom: 0;
+  .add-to-cart {
+    button {
+      height: 55px;
+      width: 250px;
+      background-color: hsl(26, 100%, 55%);
+      border: none;
+      border-radius: 10px;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      img {
+        margin-right: 10px;
+      }
+      color: white;
+      font-weight: 700;
+      cursor: pointer;
+      transition: 100ms ease;
+      &:hover {
+        background-color: hsl(26, 100%, 55%, 50%);
+      }
+    }
   }
-`
+  @media (max-width: 499px) {
+    .quantity {
+      width: 100%;
+      margin-bottom: 20px;
+      margin-right: 0;
+    }
+    .add-to-cart {
+      width: 100%;
+      button {
+        width: 100%;
+      }
+    }
+  }
+`;
+
 
 export default AmountButtons
