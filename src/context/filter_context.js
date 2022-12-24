e78bea5d8a97cad23d3b734a185ece01c387c743
greatkene <great.ihevueme@tekparadise.com>
2022-12-24 12:37:19 +0100

    sort functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -31,6 +31,10 @@ export const FilterProvider = ({ children }) => {
     dispatch({type: LOAD_PRODUCTS, payload: products})
   }, [products])
 
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort])
+
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW })
   }
