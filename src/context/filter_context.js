000269133f1a432ab3c1913047663b3a33134c21
greatkene <great.ihevueme@tekparadise.com>
2022-12-27 15:29:20 +0100

    feat: filter search text setup

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,7 +16,17 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
-  sort: 'price-lowest'
+  sort: 'price-lowest',
+  filters:{
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  }
 }
 
 const FilterContext = React.createContext()
@@ -32,8 +42,9 @@ export const FilterProvider = ({ children }) => {
   }, [products])
 
   useEffect(() => {
+    dispatch({type: FILTER_PRODUCTS})
     dispatch({ type: SORT_PRODUCTS })
-  }, [products, state.sort])
+  }, [products, state.sort, state.filters])
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW })
@@ -48,8 +59,26 @@ export const FilterProvider = ({ children }) => {
     dispatch({type: UPDATE_SORT, payload: value})
   }
 
+  const updateFilters = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+    dispatch({type: UPDATE_FILTERS, payload: {name, value}})
+  }
+
+  const clearFilters = () => {
+
+  }
+
   return (
-    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort}}>
+    <FilterContext.Provider 
+      value={{
+        ...state, 
+        setGridView, 
+        setListView,
+        updateSort,
+        updateFilters,
+        clearFilters
+      }}>
       {children}
     </FilterContext.Provider>
   )
